5b8cac4e7b6d1f161fc9451e9de43a584ce77cec
DanielShumbusho <danielshb14@gmail.com>
2025-12-15 10:29:40 +0100

    Initial blank screen with text

---
+++ expo-basics/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Hello Expo 👋</Text>
+    </View>
+  );
+}
