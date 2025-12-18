0af0b7254d5ee6f95e0c93d4d264dc665a4c6451
DanielShumbusho <danielshb14@gmail.com>
2025-12-18 12:06:20 +0100

    played around modifying the default tab bar to learn the basics before attacking a custom tabBar

--- expo-basics/app/(tabs)/_layout.tsx
+++ expo-basics/app/(tabs)/_layout.tsx
@@ -1,27 +1,60 @@
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import CustomTabBar from '@/components/CustomTabBar';
+import Animated, { FadeIn } from 'react-native-reanimated';
 
-export default function TabLayout() {
-  return (
-    <Tabs
-      tabBar={(props) => <CustomTabBar {...props} />}
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          position: 'absolute',
-          height: 0,                 // 🔑 REMOVE SPACE
-          backgroundColor: 'transparent',
-          borderTopWidth: 0,
-          elevation: 0,
-        },
-        tabBarItemStyle: {
-          display: 'none',           // 🔑 HIDE DEFAULT ITEMS
-        },
-      }}
-    >
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="explore" />
-      <Tabs.Screen name="profile" />
-    </Tabs>
-  );
+export default function TabLayout(){
+    return(
+        <Tabs screenOptions = {{
+            headerShown: false,
+            tabBarActiveTintColor: '#4f46e5',
+            tabBarInactiveTintColor: '#999',
+            tabBarStyle:{
+                backgroundColor: '#c8e6faff',
+                borderTopWidth: 0,
+                elevation: 0,
+                height: 70,
+                borderRadius: 30,
+                marginHorizontal: 16,
+                marginBottom: 16,
+                position: 'absolute',
+            }
+            }}>
+            <Tabs.Screen
+                name='index'
+                options = {{
+                    title: 'Home',
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Animated.View
+                        style={{
+                        transform: [{ scale: focused ? 1.2 : 1 }],
+                        }}
+                    >
+                    <IconSymbol 
+                        size={30} 
+                        // name={focused ? 'house.fill' :'house' }
+                        name='house.fill'
+                        color={color} /></Animated.View>),
+                }}
+            />
+            <Tabs.Screen
+                name='profile'
+                options = {{
+                    title: 'Profile',
+                    tabBarIcon: ({ color, focused }) => (
+                    <Animated.View
+                        style={{
+                        transform: [{ scale: focused ? 1.2 : 1 }],
+                        }}
+                    >
+                        <Ionicons
+                        size={28}
+                        name={focused ? 'person' : 'person-outline'}
+                        color={color}
+                        />
+                    </Animated.View>),
+                }}
+            />
+        </Tabs>
+    )
 }
